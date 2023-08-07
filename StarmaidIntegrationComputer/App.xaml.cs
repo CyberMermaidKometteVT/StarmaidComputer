8fef2aca400133302c86662db922de4e70dfddf2
Komette <cybermermaidkomettevt@gmail.com>
2023-08-07 04:03:39 -0400

    Handling top-level exceptions - logging them and Thalassa is mentioning what happened.

--- StarmaidIntegrationComputer/App.xaml.cs
+++ StarmaidIntegrationComputer/App.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Threading;
 using System.Windows;
 
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using StarmaidIntegrationComputer.Thalassa.SpeechSynthesis;
 
 namespace StarmaidIntegrationComputer
 {
@@ -12,6 +16,7 @@ namespace StarmaidIntegrationComputer
     public partial class App : Application
     {
         private ServiceProvider serviceProvider;
+        private readonly ILogger<App> logger;
 
         public App()
         {
@@ -20,11 +25,47 @@ namespace StarmaidIntegrationComputer
             Startup startup = new Startup();
 
             serviceProvider = startup.ConfigureServices();
+            logger = serviceProvider.GetService<ILogger<App>>();
         }
         private void OnStartup(object sender, StartupEventArgs a)
         {
-            var mainWindow = serviceProvider.GetRequiredService<IntegrationComputerMainWindow>();
-            mainWindow.Show();
+            try
+            {
+                var mainWindow = serviceProvider.GetRequiredService<IntegrationComputerMainWindow>();
+                mainWindow.Show();
+
+                Application.Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
+            }
+            catch (Exception ex)
+            {
+                HandleTopLevelException(ex);
+            }
+        }
+
+        private void Current_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            HandleTopLevelException(e.Exception);
+        }
+
+        private void HandleTopLevelException(Exception ex)
+        {
+            logger.LogCritical($"UNHANDLED EXCEPTION: {ex.Message}{Environment.NewLine}{ex.StackTrace}");
+
+            Exception? lastException = ex.InnerException;
+            while (lastException != null)
+            {
+                logger.LogCritical($"Inner exception: {ex.Message}{Environment.NewLine}{ex.StackTrace}");
+                lastException = lastException.InnerException;
+            }
+
+            var speechComputer = serviceProvider.GetService<SpeechComputer>();
+            speechComputer.SpeechCompletedHandlers.Add(() => throw ex);
+            speechComputer.Speak($"The Starmaid Integration Computer, which runs Thalassa, just crashed. It's a {ex.GetType()}, see the log for more details.");
+            while (speechComputer.IsSpeaking)
+            {
+                Thread.Sleep(1000);
+            }
+
         }
     }
 }
