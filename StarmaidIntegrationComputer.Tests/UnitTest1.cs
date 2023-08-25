077e9641aa35c853b305838df919937033eef831
Komette <cybermermaidkomettevt@gmail.com>
2023-08-25 00:46:32 -0400

    committing straight to main because I'm awful: adding unit test project

---
+++ StarmaidIntegrationComputer.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StarmaidIntegrationComputer.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DoesTheTestRunnerWork()
+        {
+            Assert.IsTrue(true);
+            Assert.IsFalse(false);
+        }
+    }
+}
