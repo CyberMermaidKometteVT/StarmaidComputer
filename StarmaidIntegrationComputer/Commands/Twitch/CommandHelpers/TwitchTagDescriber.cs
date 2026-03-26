afd3c46f0c96336ae125bfbae9e3020399e2bc95
Komette <cybermermaidkomettevt@gmail.com>
2026-03-26 18:00:53 -0400

    Adding more tag keywords

--- StarmaidIntegrationComputer/Commands/Twitch/CommandHelpers/TwitchTagDescriber.cs
+++ StarmaidIntegrationComputer/Commands/Twitch/CommandHelpers/TwitchTagDescriber.cs
@@ -7,14 +7,13 @@ namespace StarmaidIntegrationComputer.Commands.Twitch.CommandHelpers
 {
     internal static  class TwitchTagDescriber
     {
-
-        private static IReadOnlyList<string> exactQueerLabels = new List<string> { "LGBTQIAPLUS", "QUEER", "LESBIAN", "GAY", "GAYMER", "BISEXUAL", "BI", "PANSEXUAL", "INTERSEX", "ACE", "ASEXUAL", "AROMANTIC", "GRAYACE", "GREYACE", "GENDERFLUID", "TRANSBIAN", "NONBINARY", "ENBY", "NB", "GENDERSPICY", "FRIENDOFDOROTHY", "FRIENDOFMARA" }.AsReadOnly();
-        private static IReadOnlyList<string> transLabels = new List<string> { "TRANSGENDER", "TRANSBIAN", "TRANSMAN", "TRANSWOMAN" }.AsReadOnly();
+        private static IReadOnlyList<string> exactQueerLabels = new List<string> { "LGBTQIAPLUS", "LGBT", "LGBTQ", "LGBTQIA", "LGBTQIA+", "QUEER", "LESBIAN", "GAY", "GAYMER", "BISEXUAL", "BI", "PANSEXUAL", "INTERSEX", "ACE", "ASEXUAL", "AROMANTIC", "GRAYACE", "GREYACE", "GENDERFLUID", "TRANSBIAN", "NONBINARY", "ENBY", "NB", "GENDERSPICY", "FRIENDOFDOROTHY", "FRIENDOFMARA" }.AsReadOnly();
+        private static IReadOnlyList<string> transLabels = new List<string> { "TRANSGENDER", "TRANSBIAN", "TRANSMAN", "TRANSWOMAN", "TRANS" }.AsReadOnly();
         private static IReadOnlyList<string> vtuberLabels = new List<string> { "VTUBER", "PNGTUBER", "LEWDTUBER", "GIFTUBER" }.AsReadOnly();
         private static IReadOnlyList<string> exactArtistLabels = new List<string> { "ART", "ARTIST", "PAINTER", "PAINTING", "MINIATUREPAINTING", "DIGITALARTIST" }.AsReadOnly();
         private static IReadOnlyList<string> musicianLabels = new List<string> { "SONGWRIT", "MUSICIAN", "UKELELE", "GUITAR", "PIANO", "VIOLIN", "FLUTE", "FLOUTIST",  }.AsReadOnly();
-        private static IReadOnlyList<string> eighteenPlusLabels = new List<string> { "18PLUS", "NOMINORS", "ADULT", "LEWDTUBER" }.AsReadOnly();
-        private static IReadOnlyList<string> neurodivergentLabels = new List<string> { "ADHD", "ASPERGER", "ASPERGERS", "ASPERGER'S", "AUTISTIC", "AUTISM", "AUDHD", "ANXIETY", "ANXIOUS", "BIPOLAR", "BPD", "BORDERLINE", "DEPRESSED", "DEPRESSIVE", "DEPRESSION", "DID", "DISASSOCIATIVE", "DISASSOCIATIVEIDENTITY", "DISASSOCIATIVEIDENTITYDISORDER", "DOWNSYNDROME", "DYSCALCULIA", "DYSLEXIA", "EPILEPSY", "EPILEPTIC", "MENTALHEALTH", "MISOPHONIA", "OBSESSIVECOMPULSIVEDISORDER", "OCD", "PTSD", "CPTSD", "C-PTSD", "PARKINSON", "PARKINSONS", "PARKINSON'S", "SCHIZOPHRENIA", "SYNESTHESIA", "TOURETTE", "TOURETTES", "TOURETTE'S" }.AsReadOnly();
+        private static IReadOnlyList<string> eighteenPlusLabels = new List<string> { "18PLUS", "NOMINORS", "ADULT", "LEWDTUBER", "NSFW" }.AsReadOnly();
+        private static IReadOnlyList<string> neurodivergentLabels = new List<string> { "ADHD", "ASPERGER", "ASPERGERS", "ASPERGER'S", "AUTISTIC", "AUTISM", "AUDHD", "ANXIETY", "ANXIOUS", "BIPOLAR", "BPD", "BORDERLINE", "DEPRESSED", "DEPRESSIVE", "DEPRESSION", "DID", "DISASSOCIATIVE", "DISASSOCIATIVEIDENTITY", "DISASSOCIATIVEIDENTITYDISORDER", "DOWNSYNDROME", "DYSCALCULIA", "DYSLEXIA", "EPILEPSY", "EPILEPTIC", "MENTALHEALTH", "MISOPHONIA", "OBSESSIVECOMPULSIVEDISORDER", "OCD", "PTSD", "CPTSD", "C-PTSD", "OSDD", "PARKINSON", "PARKINSONS", "PARKINSON'S", "SCHIZOPHRENIA", "SYNESTHESIA", "TOURETTE", "TOURETTES", "TOURETTE'S" }.AsReadOnly();
 
         private static IReadOnlyList<char> vowels = new List<char> ("aeiou").AsReadOnly();
 
